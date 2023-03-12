8267b445696837310b97af8a76eaf976295019d2
sief <sief.eldeen@revca.tech>
2023-03-12 14:24:28 +0200

    'files'

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Router from './Router.jsx'
+
+function App() {
+  return (
+    <Router/>
+  )
+}
+
+export default App
---
+++ src/pages/home_page/Home_page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home_page() {
+  return (
+    <div>Home_page</div>
+  )
+}
+
+export default Home_page
