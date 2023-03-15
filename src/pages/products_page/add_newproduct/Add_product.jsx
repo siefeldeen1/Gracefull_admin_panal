84833dbf9e74c809afe1f1c226a798484dff94cd
sief <sief.eldeen@revca.tech>
2023-03-15 20:39:18 +0200

    files

--- src/pages/products_page/add_newproduct/Add_product.jsx
+++ src/pages/products_page/add_newproduct/Add_product.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Add_product.css'
 import Sidebar from '../../sidebar/Sidebar'
 import Input from '../../../components/inputs/Input'
 
 
 function Add_product() {
-
+    const [Prodcut_Title, setProdcut_Title] = useState('    ')
 
     const post = ()=>{
         fetch('http://localhost:8082/',{
@@ -40,18 +40,33 @@ function Add_product() {
 
             <div className='form_cont'>
                 <div className='upload_img_cont'>
-                    <div>Upload your product image</div>
+                    <h2>Upload your product image</h2>
                 
                     <div className='upload_cont' onClick={click_changer}>
                         <div className='upload_title'>Upload image</div>
                         <input type="file" className='input_files' />
                     </div>
                 </div>
-                    <div>
-                        <div>
-                            <Input label={"product title"}/>
-                        </div>
+
+                <div className='lower_Part_of_add_prod'>
+               
+                    <div className='grid_from'>
+                      
+                            <Input onChange={(e)=>{setProdcut_Title(e.target.value)}} placeholder={'Enter your product name'} label={"Product title"}/>
+                            <Input onChange={(e)=>{setProdcut_Title(e.target.value)}} placeholder={'Enter your brand name'} label={"Brand Name"}/>
+                            <Input onChange={(e)=>{setProdcut_Title(e.target.value)}} placeholder={'Enter your product colour'} label={"Product Colour"}/>
+                            <Input onChange={(e)=>{setProdcut_Title(e.target.value)}} placeholder={'Enter which skin type should use your product'} label={"Skin type"}/>
+                            <Input onChange={(e)=>{setProdcut_Title(e.target.value)}} placeholder={'Enter your item form'} label={"Item form"}/>
+                            <Input onChange={(e)=>{setProdcut_Title(e.target.value)}} placeholder={'Enter your product size'} label={"Product Size"}/>
+                           
                     </div>
+                    
+                    <div className='text_area_class'>
+                        <label htmlFor="">Product Description </label>
+                        <textarea className='text_area_for_discribtion' placeholder='Add your product description' cols="30" rows="10"></textarea>
+                    </div>
+                    <button className='add_prod_btn_class'>Add product</button>
+                </div>
             </div>
       
       </div>
